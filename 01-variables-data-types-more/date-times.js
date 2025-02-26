96799d48f0ca4159e27cec1d7b83fe8a58444d09
Christian <kalonjic34@gmail.com>
2025-02-26 20:05:34 +0200

    Add date-times.js to demonstrate various Date object instantiations and methods

---
+++ 01-variables-data-types-more/date-times.js
@@ -0,0 +1,18 @@
+let d;
+
+d = new Date();
+
+d = new Date(2021, 5, 12, 12, 30, 0)
+
+d = new Date('2021-07-10T12:30:00')
+
+d = new Date('07-10-2021 12:30:00')
+
+d = new Date('2022-07-10')
+
+d = Date.now()
+
+d = new Date('07-10-2022 12:30:00')
+d = d.getTime()
+
+console.log(d);
