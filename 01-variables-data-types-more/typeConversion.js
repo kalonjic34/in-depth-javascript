3d309d94aec69ac3caf79f49a9aff0f770a5ac0f
Christian <kalonjic34@gmail.com>
2025-02-26 18:26:36 +0200

    Add typeConversion.js with examples of type conversions in JavaScript

---
+++ 01-variables-data-types-more/typeConversion.js
@@ -0,0 +1,18 @@
+let amount = '99.5';
+
+// Change string to number
+// amount = parseFloat(amount)
+// amount = +amount
+// amount = Number(amount)
+
+// Change number to string
+// amount = amount.toString();
+// amount = String(amount)
+
+// Change string to decimal
+// amount = parseFloat(amount)
+
+// Convert number to boolean
+amount = Boolean(amount);
+
+console.log(amount, typeof amount);
