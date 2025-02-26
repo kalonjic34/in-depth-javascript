1648734d93b85c8e79662b04996cb068724f30e1
Christian <kalonjic34@gmail.com>
2025-02-26 18:26:51 +0200

    Add operators.js with examples of arithmetic, comparison, and assignment operators

---
+++ 01-variables-data-types-more/operators.js
@@ -0,0 +1,54 @@
+// 1. Arithmetic Operator
+
+let x;
+
+x = 5 + 5;
+x = 5 - 5;
+x = 5 * 5;
+x = 5 / 5;
+x = 5 % 5;
+
+// Concatenation
+
+x = "Hello" + " world";
+
+console.log(x);
+
+// Exponent
+x = 2 ** 3;
+
+// Increment
+x = 1;
+x++;
+
+// Decrement
+x = x - 1;
+x--;
+
+// Assignment Operator
+
+x = 10;
+
+x += 5;
+x -= 5;
+x *= 5;
+x /= 5;
+x **= 5;
+
+x = x + 1;
+
+console.log(x);
+
+// Comparison Operator
+
+x = 2 == 1;
+x = 2 === 2;
+x = 2 != 2;
+x = 2 !== 2;
+
+x = 10 > 5;
+x = 10 < 5;
+x = 10 <= 5;
+x = 10 >= 5;
+
+console.log(x);
