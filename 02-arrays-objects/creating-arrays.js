f3728782332af1f20d21d629def5acf399456bb8
Christian <kalonjic34@gmail.com>
2025-02-27 09:29:31 +0200

    Add examples of array creation and manipulation in creating-arrays.js

---
+++ 02-arrays-objects/creating-arrays.js
@@ -0,0 +1,32 @@
+let x;
+
+// Array Literal
+
+const numbers = [12,43,33,29,67,234]
+const mixed = [12, 'hello', true, null]
+
+// Array Constructor
+
+const fruits = new Array('apple', 'grape', 'orange')
+
+x = numbers[0]
+
+x = numbers[0] + numbers[3]
+
+x = `My favorite fruit is an ${fruits[1]}`
+
+x = numbers.length
+
+fruits[2] ='pear'
+
+fruits[3] = 'strawberry'
+
+fruits[fruits.length] ='blueberry'
+fruits[fruits.length] ='peach'
+
+x = fruits
+
+console.log(x);
+
+
+
