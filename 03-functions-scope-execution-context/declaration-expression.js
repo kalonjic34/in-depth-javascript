4dc66667cbe822685446ca8b5a141e77a912c0a3
Christian <kalonjic34@gmail.com>
2025-02-27 14:26:35 +0200

    Add declaration-expression.js demonstrating function declarations and expressions

---
+++ 03-functions-scope-execution-context/declaration-expression.js
@@ -0,0 +1,13 @@
+// function declaration
+
+function addDollorSign(value){
+    return '$' +value
+}
+console.log(addDollorSign(100));
+
+// function expression
+
+const addPlusSign = function(value){
+    return '+' +value
+}
+console.log(addPlusSign(200));
