b3f0442588d6f2804be0a9abe32133456c035f75
Christian <kalonjic34@gmail.com>
2025-02-27 14:50:09 +0200

    Add invoked-function-expressions.js demonstrating IIFE usage and arrow functions

---
+++ 03-functions-scope-execution-context/invoked-function-expressions.js
@@ -0,0 +1,14 @@
+(function () {
+    const user = 'Chris'
+    console.log(user);
+    const hello = () => console.log('Hello from the IIFE');
+    hello()
+})()
+
+(function(name){
+    console.log('Hello '+name);
+})('Sam')
+
+(function hello(){
+    console.log('Hello');
+})()
