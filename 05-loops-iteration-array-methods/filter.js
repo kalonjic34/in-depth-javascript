4d7fa631de1fa0b541570a999cb619ffc05eaa7f
Christian <kalonjic34@gmail.com>
2025-03-03 12:15:01 +0200

    Add filter examples to demonstrate filtering companies by category and duration

--- 05-loops-iteration-array-methods/filter.js
+++ 05-loops-iteration-array-methods/filter.js
@@ -21,3 +21,35 @@ numbers.forEach(number =>{
 })
 
 console.log(evenNumbers3);
+
+const companies = [
+
+    { name: 'Company One', category: 'Finance', start: 1981, end: 2004 },
+    { name: 'Company Two', category: 'Retail', start: 1992, end: 2008 },
+    { name: 'Company Three', category: 'Auto', start: 1999, end: 2007 },
+    { name: 'Company Four', category: 'Retail', start: 1989, end: 2010 },
+    { name: 'Company Five', category: 'Technology', start: 2009, end: 2014 },
+
+]
+
+// Get only retail companies
+
+const companyRetails = companies.filter((company) =>{
+    return company.category === 'Retail'
+})
+
+console.log(companyRetails);
+
+// Get companies that started in or after 1980 and ended in or before 2005
+
+const earlyCompany = companies.filter((company)=>{
+    return company.start >= 1980 && company.end <= 2005
+})
+
+console.log(earlyCompany);
+
+// Get companies that lasted 10 years or more 
+const longCompanies = companies.filter((company) =>{
+    return company.end - company.start >= 10
+})
+console.log(longCompanies);
