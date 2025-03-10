6409b9dabf5fd11bb370763e760d4600a8b85a7e
Christian <kalonjic34@gmail.com>
2025-03-10 16:09:34 +0200

    Implement arithmetic functions and logging for user input in app.js

--- 06-more-practice-basics/assets/scripts/app.js
+++ 06-more-practice-basics/assets/scripts/app.js
@@ -1,9 +1,45 @@
 const defaultResult = 0
 let currentResult = defaultResult;
 
+function getUserNumberInput(){
+    return parseFloat(usrInput.value)
+}
+
+function createAndWriteLog(operator,resultBeforeCalc, calcNumber){
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
+    outputResult(currentResult, calcDescription);
+
+}
+
 function add(){
-    currentResult = currentResult + parseInt(userInput.value);
-    outputResult(currentResult, '');
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult + enteredNumber;
+    createAndWriteLog('+',initialResult, enteredNumber)
 };
 
+function substract(){
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult - enteredNumber;
+    createAndWriteLog('-',initialResult, enteredNumber)
+}
+
+function multiply(){
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult * enteredNumber;
+    createAndWriteLog('*',initialResult, enteredNumber)
+}
+
+function divide(){
+    const enteredNumber = getUserNumberInput()
+    const initialResult = currentResult
+    currentResult = currentResult / enteredNumber;
+    createAndWriteLog('/',initialResult, enteredNumber)
+}
+
 addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',substract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
