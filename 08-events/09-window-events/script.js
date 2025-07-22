7a08f56077b3f7d1af5c83c9740079c74e307543
Christian <kalonjic34@gmail.com>
2025-07-22 16:29:56 +0200

    Implement window load events, update document title

---
+++ 08-events/09-window-events/script.js
@@ -0,0 +1,15 @@
+// window.onload = function () {
+//     document.querySelector('h1').textContent = 'Hello World'
+// }
+
+window.addEventListener('load', ()=>{
+    console.log('Page loaded');
+})
+
+window.addEventListener('DOMContentLoaded', ()=>{
+    console.log('DOM Loaded');
+})
+
+console.log('Run me');
+
+document.querySelector('h1').innerText = 'Hell'
